2110305dffdcd5fd54239a796691e2f0c7d63592
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-06 13:29:05 +0900

    롤링페이퍼생성API

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -3,4 +3,28 @@ const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 const { Posts } = require("../models");
 
+router.post("/posts",authMiddleware, async(req,res)=>{
+    try{
+    const {userId} = res.locals.user
+    const {title, content} = req.body
+    const RE_TITLE = 
+    const RE_CONTENT = 
+    if(title===null&&content===null){
+     return res.status(400).json({errorMessage : "제목 또는 내용을 입력하세요."})
+    }
+    
+    await Posts.create({
+        postId : postId
+        UserId : userId,
+        title : title,
+        content : content,
+    })
+return res.status(200).json({message : "롤 생성에 성공하였습니다."})
+}catch(err){
+    res.status(400).json({errorMessage : "롤 생성에 실패하였습니다."})
+}
+
+})
+
+
 module.exports = router;
