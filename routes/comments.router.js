47f80c71aafb64dd9fa33fc68e8c04a631ccaf59
kimchaeminthegreat <125367831+kimchaeminthegreat@users.noreply.github.com>
2023-05-06 15:39:44 +0900

    [채민] 롤 수정 : put, /api/posts/:postId, 롤링페이퍼 상세화면(p.15)

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,7 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authMiddleware = require("../middlewares/auth-middleware");
-const { Comments } = require("../models");
+const authMiddleware = require('../middlewares/auth-middleware');
+const { Op } = require('sequelize');
+const { Comments, Posts } = require('../models');
+
+
 
 
 module.exports = router;
--- routes/posts.router.js
+++ routes/posts.router.js
@@ -3,4 +3,37 @@ const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 const { Posts } = require("../models");
 
+// [채민] 롤 수정 : put, /api/posts/:postId, 롤링페이퍼 상세화면(p.15)
+router.put("posts/:postId", authMiddleware,
+    async (req, res) => { 
+    try {
+      const { postId } = req.params;
+      const { title, content } = req.body;
+      const { nickname, UserId } = res.locals.user;
+
+      const post = await Posts.findOne({
+        where: { postId: postId } 
+      });
+
+      if (!post) return res.status(404).json({ errorMessage: "게시글이 존재하지 않습니다." });
+
+      if (post.nickname !== nickname)
+        return res.status(403).json({ errorMessage: "게시글 수정의 권한이 존재하지 않습니다." });
+
+      await Posts.update(
+        { title, content },
+        { where: { postId: postId } }
+      ).catch((err) => {
+        return res.status(401).json({ errorMessage: "게시글이 정상적으로 수정되지 않았습니다." });
+      });
+
+      return res.status(200).json({ message: "게시글을 수정하였습니다." });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ errorMessage: "게시글 수정에 실패하였습니다." });
+    }
+  }
+);
+
+
 module.exports = router;
