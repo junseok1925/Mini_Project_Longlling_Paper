d952999d22bf362e3b2b158649a871bce9365a8a
kimchaeminthegreat <125367831+kimchaeminthegreat@users.noreply.github.com>
2023-05-09 19:31:48 +0900

    app.js : cors 수정하였습니다.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const usersRouter = require("./routes/users.router");
 const postsRouter = require("./routes/posts.router");
 const commentsRouter = require("./routes/comments.router");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3013;
@@ -12,20 +13,28 @@ app.use(cookieParser());
 app.use('/api', usersRouter);
 app.use('/api', postsRouter);
 app.use('/api', commentsRouter);
-app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*", // 모든 출처 허용 옵션. true 를 써도 된다.
 
-//cors
-app.use(cors({
-  origin: "*",
-  credentials: true,
-  optionsSuccessStatus: 200,
-}));
+    allowedHeaders: ["content-Type", "Authorization"],
+    exposedHeaders: ["content-Type", "Authorization"],
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
+    credential: "true",
+  })
+);
+
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
 })
-
-// app.get('/', function(req,res){
-//   res.sendFile(path.join(__dirname,'Project_Longlling_Papper/public/index.html'));
-// })
-
