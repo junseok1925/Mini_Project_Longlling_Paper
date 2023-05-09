cb3339e631714c124003498f64342a5e7a19d2fc
junseok1925 <rkdwnstjr1925@naver.com>
2023-05-10 02:35:31 +0900

    포트번호 수정

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const commentsRouter = require('./routes/comments.router');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3013;
+const PORT = 3000;
 
 app.use(
   cors({
@@ -20,6 +20,41 @@ app.use(cookieParser());
 app.use('/api', usersRouter);
 app.use('/api', postsRouter);
 app.use('/api', commentsRouter);
+
+
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+});
+
+
+// const express = require('express');
+// const path = require('path'); // 이 줄을 추가하세요.
+// const cookieParser = require('cookie-parser');
+// const usersRouter = require('./routes/users.router');
+// const postsRouter = require('./routes/posts.router');
+// const commentsRouter = require('./routes/comments.router');
+// const cors = require('cors');
+
+// const app = express();
+// const PORT = 3013;
+
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3001',
+//     credentials: true,
+//   }),
+// );
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use('/api', usersRouter);
+// app.use('/api', postsRouter);
+// app.use('/api', commentsRouter);
+
+// // 정적 파일 제공 설정을 추가하세요.
+// app.use(express.static("build"));
+
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
