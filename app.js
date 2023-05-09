f8db951a8468611e42a275d5ce5ed3d1317d00ff
junseok1925 <rkdwnstjr1925@naver.com>
2023-05-09 20:45:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use('/api', usersRouter);
 app.use('/api', postsRouter);
 app.use('/api', commentsRouter);
 
+// t수정수정ㅈ수정
 app.options("*", cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
