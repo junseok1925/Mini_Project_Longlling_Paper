ff3f5e897f6639ea0ad3f4550608b5e10e4ee562
kimchaeminthegreat <125367831+kimchaeminthegreat@users.noreply.github.com>
2023-05-10 23:06:52 +0900

    Feature/Api Put 롤링페이퍼 수정하기 부분 업데이트

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -28,23 +28,23 @@ class PostController {
       const findOnePost = await this.postService.findOnePost(postId);
       if (!findOnePost) {
         return res
-          .status(400)
-          .json({ errorMessage: '수정할 롤링페이퍼가 없음' });
+          .status(404)
+          .json({ errorMessage: '롤링페이퍼가 존재하지 않습니다.' });
       }
       if (userId !== findOnePost.UserId) {
-        return res.status(400).json({ errorMessage: '수정 권한 없음' });
+        return res.status(403).json({ errorMessage: '롤링페이퍼 수정 권한이 없습니다.' });
       }
       if (!title && !content) {
         return res
-          .status(400)
-          .json({ errorMessage: '제목 또는 내용을 입력하세요' });
+          .status(402)
+          .json({ errorMessage: '제목 또는 내용을 입력하세요!' });
       }
 
       await this.postService.putPost(postId, title, content);
-      return res.status(200).json({ message: '롤링페이퍼 수정성공' });
+      return res.status(200).json({ message: '롤링페이퍼를 수정하였습니다.' });
     } catch (err) {
       console.error(err);
-      return res.status(400).json({ errorMessage: '롤링페이퍼 수정실패' });
+      return res.status(400).json({ errorMessage: '롤링페이퍼 수정에 실패하였습니다.' });
     }
   };
   //롤페이퍼 삭제
